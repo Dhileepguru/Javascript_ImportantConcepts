f49d8fed1f346238c0fe58115f36babe08f9fa48
Dhileepguru <dhileepguru497@gmail.com>
2026-05-30 08:31:22 +0530

    Object struct

--- assets/Script/script.js
+++ assets/Script/script.js
@@ -26,3 +26,16 @@ let arr=[1,2,3,4,5];
 
 const [x,y,z,...restVal]=arr;
 console.log(x,y,z,restVal);
+
+//object destruct
+
+let objVal= {
+    firstName:'dhileep',
+    lastName:'D',
+    Age:22,
+    mob:1234567890
+}
+
+const {firstName,lastName,Age: VoteAge,...restVal2}=objVal;
+
+console.log(firstName,lastName,VoteAge,restVal2);
