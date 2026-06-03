03a26ce5bf8689d0152f57c8863e08d7360fe33f
Dhileepguru <dhileepguru497@gmail.com>
2026-06-03 08:42:11 +0530

    ArrayMethods (tostring,join,concat.splice,slice,indexof,lastindexof))

--- assets/Script/script.js
+++ assets/Script/script.js
@@ -39,3 +39,65 @@ let objVal= {
 const {firstName,lastName,Age: VoteAge,...restVal2}=objVal;
 
 console.log(firstName,lastName,VoteAge,restVal2);
+
+//Practice Destruct
+
+let arrVal=[45,32,4,3,67];
+
+const [num1,num2,num3,num4,...restval3]=arrVal;
+
+let con2=document.getElementsByClassName("con2")[0];
+
+let Ptag=document.createElement("p");
+Ptag.innerHTML=num1+num2+num3+num4+restval3;
+con2.appendChild(Ptag);
+
+//Array Methods 
+ 
+//tostring()
+
+let fruits =['apple','orange','mango']
+
+console.log(fruits);
+let ToStringVal=fruits.toString();
+console.log(ToStringVal);
+
+//Join()
+console.log(fruits.join(' and ')  );
+
+//Concat()
+
+let names=['guru','saran'];
+let names2=['Manoj'];
+
+let combinedVal=names.concat(names2);
+console.log(combinedVal);
+
+//splice
+// remove a values and add a value 
+combinedVal.splice(1,2,'dhileep','saritha');
+console.log(combinedVal);
+
+
+//slice
+// same like splice but here the original values will not be changed
+let sliceTest=combinedVal.slice(1,2);
+console.log(sliceTest);
+
+
+//indexOf()
+
+console.log(combinedVal.indexOf('dhileep'));
+
+combinedVal.push('dhileep');
+console.log(combinedVal);
+
+//lastIndexof
+
+console.log(combinedVal.lastIndexOf('dhileep'));
+
+
+
+
+
+
